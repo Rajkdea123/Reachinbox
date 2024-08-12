3b9b7c7e1f5394ecb297dc0b77dd3fb3a808250c
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2024-08-12 12:07:19 +0530

    Update MainContainer.jsx

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import CustomMail from "./CustomMail";
 import { MdOutlineExpand } from "react-icons/md";
