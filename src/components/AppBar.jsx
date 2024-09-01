e4b63b9cc452b28a6cebda1e3afa29580d0c3abe
Rajkdea123 <ravindrayadav78795@gmail.com>
2024-09-01 21:04:36 +0530

    first commit

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,7 +3,7 @@ function AppBar() {
 		<div className="border-[#25262B] bg-black border-b-2 fixed text-white h-16 w-screen flex items-center justify-center">
 			<div>
 				<img
-					src="https://s3-alpha-sig.figma.com/img/37f3/2626/1680d9613629542a2474f79f92f7c022?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fl4AzEtXSK~06lRD-V3DXYPZGX-3ok8CEebBaMpSkWQAkFWOVwabcZspMmEKNJES3i8SqT9w1c-Qt96D1-3ezTqfrM~RwaNthPHlPppqJ4fCG0oXLlENaaOubkJhpqGEVqAwJK54pmhpO0YTC5QDVSexqsQ9r2XMeBnhoLJDvYGo4GiLG-IDAY3q4Xstp5YuRtiWYG2EpThoF3R1SiQYLzYuHq0ges5bFcTw64nKinSqt0hB8A7~LcnqIvJImb0Nya-B12d1M13IxP3i0zLHkkWXM0XrvjmSVFkuS9mX82bDOqUG7A9lH~R3GH-A38NCtoyLR17HHwFxf~d9kB59hw__"
+					src="https://cutshort.io/_next/image?url=https%3A%2F%2Fcdnv2.cutshort.io%2Fcompany-static%2F65fd8656038bd7002a27e9a7%2Fuser_uploaded_data%2Flogos%2Freachinbox_ai_logo.jpeg&w=128&q=75"
 					alt="logo"
 					className="h-10"
 				/>
